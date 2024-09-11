dcbf8d9962670252eed8cfdd8dac916c4b69488d
Ojie <abhulimenraphael20@gmail.com>
2024-09-12 00:08:00 +0100

    initialised the deployment scripts

--- contracts/ETH-test-token.sol
+++ contracts/ETH-test-token.sol
@@ -4,21 +4,19 @@ pragma solidity ^0.8.20;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract JacksonToken is ERC20, Ownable, ERC20Permit {
-    constructor(address initialOwner)
+contract JacksonToken is ERC20,  ERC20Permit {
+    constructor(uint256 initialSupply)
         ERC20("JacksonToken", "JTK")
-        Ownable(initialOwner)
         ERC20Permit("JacksonToken")
     {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-        console.log("Tokens are successfully minted %s", 1000000 * 10 ** decimals());
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+        console.log("Tokens are successfully minted %s", initialSupply * 10 ** decimals());
         console.log("Contract deployed! Tokens sent to %s", msg.sender);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public  {
         _mint(to, amount);
     }
 }
