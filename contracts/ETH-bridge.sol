f27ccc873b1136e0f3e99f52c6ab4bf35f0f40b2
Ojie <abhulimenraphael20@gmail.com>
2024-09-20 21:38:07 +0100

    used chainlink on the eth bridge

--- contracts/ETH-bridge.sol
+++ contracts/ETH-bridge.sol
@@ -3,11 +3,28 @@ pragma solidity ^0.8.4;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/libraries/Client.sol";
+import {LinkTokenInterface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
 contract SourceBridge {
     IERC20 public token;
     address public admin;
     
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        string text, // The text being sent.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the CCIP message.
+    );
+
+    IRouterClient private s_router;
+
+    LinkTokenInterface private s_linkToken;
+
     event TokensLocked(
         address indexed user,
         uint256 amount,
@@ -16,10 +33,57 @@ contract SourceBridge {
     );
 
     constructor(address _token) {
-        token = IERC20(_token);  // Token to be transferred cross-chain
-        admin = msg.sender;
+        s_router = IRouterClient(_router);
+        s_linkToken = LinkTokenInterface(_link);
     }
 
+    function sendMessage(
+        uint64 destinationChainSelector,
+        address receiver,
+        string calldata text
+    ) external onlyOwner returns (bytes32 messageId) {
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver), // ABI-encoded receiver address
+            data: abi.encode(text), // ABI-encoded string
+            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array indicating no tokens are being sent
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 200_000})
+            ),
+            // Set the feeToken  address, indicating LINK will be used for fees
+            feeToken: address(s_linkToken)
+        });
+
+        // Get the fee required to send the message
+        uint256 fees = s_router.getFee(
+            destinationChainSelector,
+            evm2AnyMessage
+        );
+
+        if (fees > s_linkToken.balanceOf(address(this)))
+            revert NotEnoughBalance(s_linkToken.balanceOf(address(this)), fees);
+
+        // approve the Router to transfer LINK tokens on contract's behalf. It will spend the fees in LINK
+        s_linkToken.approve(address(s_router), fees);
+
+        // Send the message through the router and store the returned message ID
+        messageId = s_router.ccipSend(destinationChainSelector, evm2AnyMessage);
+
+        // Emit an event with message details
+        emit MessageSent(
+            messageId,
+            destinationChainSelector,
+            receiver,
+            text,
+            address(s_linkToken),
+            fees
+        );
+
+        // Return the message ID
+        return messageId;
+    }
+    
     // Lock tokens to begin the cross-chain transfer process
     function lockTokens(uint256 _amount, uint256 _targetChainId) external {
         require(_amount > 0, "Amount must be greater than 0");
