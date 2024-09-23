ffdaa53346fb4ae69abaf6ae8defde589a48f2ef
Ojie <abhulimenraphael20@gmail.com>
2024-09-23 19:54:45 +0100

    Optimized the sender bridge

--- contracts/ETH-bridge.sol
+++ contracts/ETH-bridge.sol
@@ -26,10 +26,13 @@ contract SourceBridge {
     LinkTokenInterface private s_linkToken;
 
     event TokensLocked(
-        address indexed user,
-        uint256 amount,
-        uint256 targetChainId,
-        bytes32 transactionId
+      bytes32 indexed messageId, // The unique ID of the CCIP message.
+      uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+      address receiver, // The address of the receiver on the destination chain.
+      address feeToken, // the token address used to pay CCIP fees.
+      uint256 fees, // The fees paid for sending the CCIP message.
+      uint256 amount, // amount of tokens to be sent
+      string text, // The text being sent.
     );
 
     constructor(address _token) {
@@ -42,6 +45,7 @@ contract SourceBridge {
         address receiver,
         string calldata text
     ) external onlyOwner returns (bytes32 messageId) {
+        require(_amount > 0, "Amount must be greater than 0");
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
         Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
             receiver: abi.encode(receiver), // ABI-encoded receiver address
@@ -70,33 +74,21 @@ contract SourceBridge {
         // Send the message through the router and store the returned message ID
         messageId = s_router.ccipSend(destinationChainSelector, evm2AnyMessage);
 
+        // Transfer tokens from user to the bridge contract
+        token.transferFrom(msg.sender, address(this), _amount);
+
         // Emit an event with message details
         emit MessageSent(
             messageId,
             destinationChainSelector,
             receiver,
-            text,
             address(s_linkToken),
-            fees
+            fees,
+            amount,
+            text
         );
 
         // Return the message ID
         return messageId;
-    }
-    
-    // Lock tokens to begin the cross-chain transfer process
-    function lockTokens(uint256 _amount, uint256 _targetChainId) external {
-        require(_amount > 0, "Amount must be greater than 0");
-
-        // Transfer tokens from user to the bridge contract
-        token.transferFrom(msg.sender, address(this), _amount);
-
-        // Generate unique transaction ID
-        bytes32 transactionId = keccak256(
-            abi.encodePacked(msg.sender, _amount, block.timestamp)
-        );
-
-        // Emit event for Chainlink Oracle to capture
-        emit TokensLocked(msg.sender, _amount, _targetChainId, transactionId);
     }
 }
