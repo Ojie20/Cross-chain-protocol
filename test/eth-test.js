7befc9cbaef6d41cf962bfa82fb2dc0f94230ff1
Nathan Useni <164697947+omormehn@users.noreply.github.com>
2024-09-27 08:49:12 +0100

    Delete test directory

--- test/eth-test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require("chai");
-const hre = require("hardhat");
-
-describe("JacksonToken", function () {
-  it("Should Deploy the token", async function (){
-    const initialSupply = 10000000
-    const JacksonToken = hre.ethers.deployContract("JacksonToken", [initialSupply])
-  });
-})
